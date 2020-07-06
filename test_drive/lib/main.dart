b5d26427ecc1b91bab1503003a729423001e732a
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2020-07-06 14:31:11 -0400

    Transformed into Cupertino theme
    Dark Mode is no good, though. The body text becomes dark-on-dark!

--- test_drive/lib/main.dart
+++ test_drive/lib/main.dart
@@ -1,17 +1,19 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
+        // FIXME: This works fine with light mode, but in dark mode, the text stays dark!
+        // The navbar text displays fine in both modes, though.
+        child: const Center(
           child: const Text('Hello World'),
         ),
       ),
